d569f5d88888c877b7359026362b1926ed77be45
Keanu.Wang <386247915@qq.com>
2020-07-23 11:31:13 +0800

    [fizzbuzz][KEANU] feature play in class FizzBuzz

---
+++ src/test/java/junit5/tdd/FizzBuzzTest.java
@@ -0,0 +1,12 @@
+package junit5.tdd;
+
+import org.junit.jupiter.api.Test;
+
+public class FizzBuzzTest {
+    @Test
+    void should_output_1_when_count_off_given_student_1(){
+        FizzBuzz fizzBuzz=new FizzBuzz();
+        int testNumber=1;
+
+    }
+}
